33e266f5c6e1654e16a7ff3e60ec5e94308fb1eb
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-09-29 08:06:10 +0600

    initial commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBlxRvRgIXS-MaxpjYLfsvHeysVwZSDYE",
+  authDomain: "burj-al-arab-86182.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-86182.firebaseio.com",
+  projectId: "burj-al-arab-86182",
+  storageBucket: "burj-al-arab-86182.appspot.com",
+  messagingSenderId: "54275009949",
+  appId: "1:54275009949:web:1c50b06038223c8137d374"
+};
+
+export default firebaseConfig;
